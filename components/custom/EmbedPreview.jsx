62db56c1fd5a05e87a50b42a75f6a3c876a64fd0
Maxim.jsx <info@maxim.cat>
2025-04-10 14:32:48 +0300

    Update EmbedPreview.jsx

--- components/custom/EmbedPreview.jsx
+++ components/custom/EmbedPreview.jsx
@@ -78,7 +78,7 @@ export default function EmbedPreview({ data }) {
             <img
               src={
                 data.image ||
-                "https://media1.tenor.com/m/p-rnE8Gy6ccAAAAd/indian-guy-funny-indian-guy.gif"
+                ""
               }
               alt="Large"
               className="rounded-md w-full object-cover"
